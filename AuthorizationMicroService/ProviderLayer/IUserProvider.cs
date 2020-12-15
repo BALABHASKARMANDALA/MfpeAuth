9cd7784b9168e72822b077c6dce71ed955b6ff98
balabhaskar <balabhaskarmandala@gmail.com>
2020-12-15 18:55:47 +0530

    first commit

---
+++ AuthorizationMicroService/ProviderLayer/IUserProvider.cs
@@ -0,0 +1,16 @@
+﻿using AuthorizationMicroService.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationMicroService.ProviderLayer
+{
+    public interface IUserProvider
+    {
+        public JwtSecurityToken LoginProvider(UserDetails user);
+        public JwtSecurityToken GenerateJSONWebToken(UserDetails userInfo);
+        public int GetUserId(UserDetails user);
+    }
+}
